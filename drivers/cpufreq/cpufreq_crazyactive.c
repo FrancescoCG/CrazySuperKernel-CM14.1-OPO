ce7a39fe049ff008f07a70039c4bf8201c2bdd49
FrancescoCG <frank-97@hotmail.gr>
2016-12-25 00:36:00 +0200

    Revert "crazyactive governor-cover for moved get_cpu_idle_time function"
    This reverts commit 1d13ed3912e12e96e84db5304355ae3597fb6587.

--- drivers/cpufreq/cpufreq_crazyactive.c
+++ drivers/cpufreq/cpufreq_crazyactive.c
@@ -186,7 +186,6 @@ static inline cputime64_t get_cpu_idle_time(unsigned int cpu,
 
 	return idle_time;
 }
-*/
 
 static void cpufreq_crazyactive_timer_resched(
 	struct cpufreq_crazyactive_cpuinfo *pcpu)
@@ -197,7 +196,7 @@ static void cpufreq_crazyactive_timer_resched(
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
 		get_cpu_idle_time(smp_processor_id(),
-				     &pcpu->time_in_idle_timestamp, io_is_busy);
+				     &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	expires = jiffies + usecs_to_jiffies(timer_rate);
@@ -235,7 +234,7 @@ static void cpufreq_crazyactive_timer_start(int cpu, int time_override)
 
 	spin_lock_irqsave(&pcpu->load_lock, flags);
 	pcpu->time_in_idle =
-		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp, io_is_busy);
+		get_cpu_idle_time(cpu, &pcpu->time_in_idle_timestamp);
 	pcpu->cputime_speedadj = 0;
 	pcpu->cputime_speedadj_timestamp = pcpu->time_in_idle_timestamp;
 	spin_unlock_irqrestore(&pcpu->load_lock, flags);
@@ -379,7 +378,7 @@ static u64 update_load(int cpu)
 	unsigned int cur_load = 0;
 #endif
 
-	now_idle = get_cpu_idle_time(cpu, &now, io_is_busy);
+	now_idle = get_cpu_idle_time(cpu, &now);
 	delta_idle = (unsigned int)(now_idle - pcpu->time_in_idle);
 	delta_time = (unsigned int)(now - pcpu->time_in_idle_timestamp);
 
